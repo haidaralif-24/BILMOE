b2884aaf1f5123c143bae276bc4c984f51594f50
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-08-31 08:07:24 +0700

    remove radial gradient background, use simple black background

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-1 flex-col bg-black font-sans">
       <Hero />
     </div>
   );
