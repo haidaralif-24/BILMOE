ed908f2908e38627c0c38ce6f055ca5869cc1b97
Haidar Ali Fawwaz Nasirodin <haidar.nasirodin@gdajogja.sch.id>
2026-08-31 17:06:44 +0700

    Add divisions section after hero

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Divisions from "@/components/Divisions";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col bg-black font-sans">
       <Hero />
+      <Divisions />
     </div>
   );
 }
